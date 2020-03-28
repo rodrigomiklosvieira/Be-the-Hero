27d5a48286114ccd8ac78429f85ae1ead0605233
Rodrigo Miklos Vieira <rodrigo.vieira67@etec.sp.gov.br>
2020-03-28 15:33:09 -0300

    Correção da página de cadastro

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -49,7 +49,7 @@ export default function Register() {
                 
                     <Link className=".back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041"/>
-                        Não tenho cadastro
+                        Voltar para página incinial.
                         </Link>
                 
                 </section>
